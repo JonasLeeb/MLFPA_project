46414550f91455038c7283a6464bb2a3caa65bd1
Jonas Leeb <ge59duw@mytum.de>
2025-05-17 11:23:57 +0200

    long scibert

---
+++ LongSciBERT.py
@@ -0,0 +1,80 @@
+from transformers import AutoTokenizer, AutoModel
+import torch
+from tqdm import tqdm
+import numpy as np
+from datasets import load_dataset
+from helper_functions import embed_chunk
+
+# Load the dataset
+ds = load_dataset("ccdv/arxiv-classification", "no_ref")
+
+# Dictionary mapping from numeric labels to class names
+arxiv_subjects = {
+    '0': 'Commutative Algebra',
+    '1': 'Computer Vision',
+    '2': 'Artificial Intelligence',
+    '3': 'Systems and Control',
+    '4': 'Group Theory',
+    '5': 'Computational Engineering',
+    '6': 'Programming Languages',
+    '7': 'Information Theory',
+    '8': 'Data Structures',
+    '9': 'Neural and Evolutionary',
+    '10': 'Statistics Theory'
+}
+
+# Check if GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+# Define batch size
+batch_size = 16
+num_chunks = 32
+
+# Initialize tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased').to(device)
+
+# Prepare the dataset
+documents = ds["train"][:]['text']
+
+# Initialize a list of lists to collect embeddings for each chunk
+cls_embeddings = [[] for _ in range(num_chunks)]
+
+# Process documents in batches
+for i in tqdm(range(0, len(documents), batch_size), desc="Processing Batches"):
+
+    batch = documents[i:i + batch_size]
+    embedded_chunk = embed_chunk(batch, max_length=512, max_chunks=num_chunks, tokenizer=tokenizer, model=model, device=device)
+    for j, chunk in enumerate(embedded_chunk):
+        cls_embeddings[j].append(chunk)
+
+# Concatenate along the batch dimension for each chunk
+cls_embeddings = np.array([torch.cat(chunks, axis=0) for chunks in cls_embeddings if chunks])
+# save bert embeddings
+np.savez_compressed("longscibert_embedding.npz", 
+                   bert_embedding=cls_embeddings)
+
+cls_embeddings_test = []
+
+# Prepare the dataset
+documents = ds["test"][:]['text']
+
+pre_truncated_docs = [doc[doc.index('Abstract'):] if 'Abstract' in doc else doc for doc in documents]
+
+# Initialize a list of lists to collect embeddings for each chunk
+cls_embeddings_test = [[] for _ in range(num_chunks)]
+
+# Process documents in batches
+for i in tqdm(range(0, len(documents), batch_size), desc="Processing Batches"):
+    batch = documents[i:i + batch_size]
+    embedded_chunk = embed_chunk(batch, max_length=512, max_chunks=num_chunks, tokenizer=tokenizer, model=model, device=device)
+    for j, chunk in enumerate(embedded_chunk):
+        cls_embeddings_test[j].append(chunk)
+
+# Concatenate along the batch dimension for each chunk
+cls_embeddings_test = np.array([torch.cat(chunks, axis=0) for chunks in cls_embeddings_test if chunks])
+
+# save bert embeddings
+np.savez_compressed("longscibert_embedding_test.npz", 
+                   bert_embedding=cls_embeddings_test)
